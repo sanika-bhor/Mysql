c375dc3ffe0688cf48c237c507372c286be0c4ec
Sanika Bhor <bhorsanika0239@gmail.com>
2024-09-05 21:33:32 +0530

    Update EcommerceSolution.sql

--- EcommerceSolution.sql
+++ EcommerceSolution.sql
@@ -25,3 +25,25 @@ create table orders (
   date date,
   total int,
   status varchar(15) );
+
+
+-- Insert products data in products table
+insert into products (productName, price) values('mobile',12000);
+insert into products (productName, price) values('laptop',75000);
+insert into products (productName, price) values('earpots',999);
+insert into products (productName, price) values('TV',17000);
+insert into products (productName, price) values('watch',5000);
+
+-- insert user credentails
+insert into credentials (email,password,name,location,age) 
+        values('bhorsanika0239@gmail.com','sanika','sanika bhor','machar',19);
+insert into credentials (email,password,name,location,age) 
+        values('sumitbhor@gmail.com','sumit','sumit bhor','manchar',18);
+
+
+-- insert orders details
+insert into orders (date,total,status) values('2024-4-7',2500,'intransmit');
+insert into orders (date,total,status) values('2024-7-4',2500,'processed');
+insert into orders (date,total,status) values('2024-2-14',8900,'processed');
+insert into orders (date,total,status) values('2024-9-4',999,'intransmit');
+
