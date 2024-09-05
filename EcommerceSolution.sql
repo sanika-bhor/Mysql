583f349c663e11fe7005b0ce82e5401bf92c89d5
Sanika Bhor <bhorsanika0239@gmail.com>
2024-09-05 21:27:15 +0530

    Update EcommerceSolution.sql

--- EcommerceSolution.sql
+++ EcommerceSolution.sql
@@ -5,11 +5,23 @@ create database shoppingCart;
 use shoppingCart;
 
 -- create table products for storing data products data
-create table products(productId int not null auto_increment primary key, productName varchar(20) ,price int);
+create table products (
+  productId int not null auto_increment primary key,
+  productName varchar(20),
+  price int );
 
 -- create table credential such that it can store users credentials details
-create table credentials(credentialId int not null auto_increment primary key,email varchar(30),password varchar(8),name varchar(30),location varchar(20),age int);
+create table credentials (
+  credentialId int not null auto_increment primary key,
+  email varchar(30),
+  password varchar(8),
+  name varchar(30),
+  location varchar(20),
+  age int );
 
 -- create table orders that store orders data
-create table orders (id int not null auto_increment primary key,date date,total int,status varchar(15));
-
+create table orders (
+  id int not null auto_increment primary key,
+  date date,
+  total int,
+  status varchar(15) );
